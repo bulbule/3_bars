0808ecae073f6d8e3deaa7722a18cde74ce2fc70
bulbule <anyagoremykina@gmail.com>
2017-02-15 00:41:56 +0100

    стилевые правки

--- bars.py
+++ bars.py
@@ -9,11 +9,11 @@ import json
 from geopy.distance import great_circle
 from geopy.geocoders import Nominatim
 
+
 def load_data(filepath):
 
-
     """ Load a filepath with cyrilic letters """
-
+    
     with open(filepath, encoding='cp1251') as data_file:
         data = json.loads(data_file.read())
         
@@ -22,10 +22,8 @@ def load_data(filepath):
 
 def get_biggest_bar(data):
 
-
     """ Returns the list of names of the bars 
-    with the largest number of seats
-    """
+    with the largest number of seats """
     
     seats = []
     biggest_bars = []
@@ -39,9 +37,9 @@ def get_biggest_bar(data):
         biggest_bars.append(data[bar]['Name'])
     
     return(biggest_bars)
+    
         
 def get_smallest_bar(data):
-    
     
     """ Returns the list of names of the bars
     with the smallest number of seats
@@ -63,9 +61,8 @@ def get_smallest_bar(data):
 
 def get_closest_bar(data, longitude, latitude):
     
-    
     """ Returns the list of names of the closest bars """
-     
+    
     my_coordinates = (longitude, latitude)
     distances = []
     closest_bars = []
@@ -75,32 +72,24 @@ def get_closest_bar(data, longitude, latitude):
         distances.append(great_circle(my_coordinates, bar_coordinates).miles)
         
     closest_bars_indexes = [i for i, j in enumerate(distances)
-                            if j == min(distances)] 
+                            if j == min(distances)]
     for index in closest_bars_indexes:
         closest_bars.append(data[index]['Name'])
     
     return(closest_bars)
-    
-    
-    
+
+
 if __name__ == '__main__':
     
-    
     data = load_data('data-2897-2016-11-23.json')
-    
     print("Biggest bars:", ";".join(map(str, get_biggest_bar(data))))
     print("Smallest bars:", ", ".join(map(str, get_smallest_bar(data))))
-    
     geolocator = Nominatim()
     value = input("Enter your gps coordinates:")
-    if not value: # if nothing is entered the location of MIPT is chosen
-        location = (geolocator.geocode("MIPT").longitude, 
+    if not value:   # if nothing is entered the location of MIPT is chosen
+        location = (geolocator.geocode("MIPT").longitude,
                     geolocator.geocode("MIPT").latitude)
     else:
         location = [float(i) for i in value.strip().split()]
-        
-            
-    print("Closest bars:", ", ".join(map(str,
-                                         get_closest_bar(data,
-                                                         location[0],
-                                                         location[1]))))
+    print("Closest bars:",
+          ", ".join(map(str, get_closest_bar(data, location[0], location[1]))))
