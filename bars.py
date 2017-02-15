05ae801c34e6ecde5d65d22684caa21488abb4ef
bulbule <anyagoremykina@gmail.com>
2017-02-15 19:10:04 +0100

    Переменная data изменена везде на более понятную.

--- bars.py
+++ bars.py
@@ -11,31 +11,29 @@ def load_data(filepath):
     if not os.path.exists(filepath):
         return None
     with open(filepath, encoding='cp1251') as data_file:
-        data = json.loads(data_file.read())
-
-    return data
+        return json.loads(data_file.read())
 
 
-def get_biggest_bar(data):
+def get_biggest_bar(bars_data):
     """ Returns the list of names of the bars
     with the largest number of seats """
 
     seats = []
     biggest_bars = []
 
-    for element in data:
+    for element in bars_data:
         seats.append(element['SeatsCount'])
 
     # indexes of the bars in data with the largest number of seats
     biggest_bars_indexes = [index for index, max_value in enumerate(seats)
                             if max_value == max(seats)]
     for bar in biggest_bars_indexes:
-        biggest_bars.append(data[bar]['Name'])
+        biggest_bars.append(bars_data[bar]['Name'])
 
     return(biggest_bars)
 
 
-def get_smallest_bar(data):
+def get_smallest_bar(bars_data):
     """ Returns the list of names of the bars
     with the smallest number of seats
     """
@@ -43,42 +41,42 @@ def get_smallest_bar(data):
     seats = []
     smallest_bars = []
 
-    for element in data:
+    for element in bars_data:
         seats.append(element['SeatsCount'])
 
     # indexes of the bars in data with the largest number of seats
     smallest_bars_indexes = [index for index, min_value in enumerate(seats)
                              if min_value == min(seats)]
     for bar in smallest_bars_indexes:
-        smallest_bars.append(data[bar]['Name'])
+        smallest_bars.append(bars_data[bar]['Name'])
 
     return(smallest_bars)
 
 
-def get_closest_bar(data, longitude, latitude):
+def get_closest_bar(bars_data, longitude, latitude):
     """ Returns the list of names of the closest bars """
 
     my_coordinates = (longitude, latitude)
     distances = []
     closest_bars = []
 
-    for element in data:
+    for element in bars_data:
         bar_coordinates = tuple(element['geoData']['coordinates'])
         distances.append(great_circle(my_coordinates, bar_coordinates).miles)
 
     closest_bars_indexes = [index for index, min_dist in enumerate(distances)
                             if min_dist == min(distances)]
     for index in closest_bars_indexes:
-        closest_bars.append(data[index]['Name'])
+        closest_bars.append(bars_data[index]['Name'])
 
     return(closest_bars)
 
 
 if __name__ == '__main__':
 
-    data = load_data('data-2897-2016-11-23.json')
-    print("Biggest bars:", ";".join(map(str, get_biggest_bar(data))))
-    print("Smallest bars:", ", ".join(map(str, get_smallest_bar(data))))
+    bars_data = load_data('data-2897-2016-11-23.json')
+    print("Biggest bars:", ";".join(map(str, get_biggest_bar(bars_data))))
+    print("Smallest bars:", ", ".join(map(str, get_smallest_bar(bars_data))))
     geolocator = Nominatim()
 
     try:
@@ -86,12 +84,11 @@ if __name__ == '__main__':
             "Enter your longitude and latitude separated by a space:")
         location = [float(number) for number in values.strip().split()]
     except ValueError:
-        location = None
-    if location is None:
         print("Something different from numbers in your input was detected.")
     else:
         if len(location) != 2:
             raise IndexError("You failed to enter exactly two numbers.")
         else:
             print("Closest bars:", ", ".join(
-                map(str, get_closest_bar(data, location[0], location[1]))))
+                map(str, get_closest_bar(bars_data,
+                                         location[0], location[1]))))
